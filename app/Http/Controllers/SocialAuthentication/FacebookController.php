44bd2085e0c69fc525775ea1b7a9ea8d01c50f36
armohanyan <ohanyyanarmen@gmail.com>
2021-11-01 18:48:57 +0400

    update files

--- app/Http/Controllers/SocialAuthentication/FacebookController.php
+++ app/Http/Controllers/SocialAuthentication/FacebookController.php
@@ -30,7 +30,7 @@ class FacebookController extends Controller
         try {
 
             $user = Socialite::driver('facebook')
-                ->fields(['social_id',  'name', 'first_name', 'last_name', 'email', 'email', 'verified']);
+                ->fields(['social_id',  'name', 'first_name', 'last_name', 'email', 'email', 'verified'])->user();
             dd($user);
             $finduser = User::where('social_id', $user->id)->first();
 
