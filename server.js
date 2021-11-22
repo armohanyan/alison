2ddc9b52503f0e0674bb7852d10b8d0f9baa10be
armohanyan <ohanyyanarmen@gmail.com>
2021-11-22 12:16:21 +0400

    Updates files

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+require('dotenv').config();
 
-// var http = require('http').Server();
-// var io = require('socket.io')(http);
-// var Redis = require('ioredis');
-// var redis = new Redis();
+const env = process.env;
 
-// redis.subscribe('chat');
-// redis.on('message', function(channel, message) {
-//     console.log('Message Recieved: ' + message);
-//     message = JSON.parse(message);
-//     io.emit(channel + ':' + message.event, message.data);
-// });
-
-// http.listen(3000, function(){
-//     console.log('Listening on Port 3000');
-// });
+require('laravel-echo-server').run({
+    authHost: env.APP_URL,
+    devMode: env.APP_DEBUG,
+    database: "redis",
+    databaseConfig: {
+        redis: {
+            host: env.REDIS_HOST,
+            port: env.REDIS_PORT,
+        }
+    }
+});
